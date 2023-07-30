a30e01e30a3458a1330c061753ed2fe27c3145e1
Diego Garcia <diegofing379@gmail.com>
2023-07-30 12:26:00 -0300

    V2.2.0 Se implementa un boton para agregar los nombres.

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,30 @@
+// Ingreso y gestión de personas y nombres - Inicio
+document.addEventListener("DOMContentLoaded", function() {    
+    function guardarNombres(){
+        const cantidadPersonas = document.getElementById("cantidadPersonas");
+        const cantPersonas = cantidadPersonas.value;
+        const nombreUno = document.getElementById("nombreUno").value;
+        const nombreDos = document.getElementById("nombreDos").value;
+    };
+    document.getElementById("ingresar").addEventListener("click", guardarNombres);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Ingreso y gestión de personas y nombres - Final
+
+// Ingreso y gestión de articulos, precios y personas que hicieron la compra - Inicio
 const gastos = [];
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -5,6 +32,10 @@ document.addEventListener("DOMContentLoaded", function () {
         const item = document.getElementById("item").value;
         const precio = document.getElementById("precio").value;
         const nombre = document.getElementById("nombre").value;
+
+        if (item.trim() === "" || isNaN(precio) || precio <= 0){
+            alert("complete los campos");
+        }
 
         const listaGastos = {
             item: item,
@@ -33,3 +64,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
     document.getElementById("agregar").addEventListener("click", agregarGasto);
 });
+// Ingreso y gestión de articulos, precios y personas que hicieron la compra - Final
