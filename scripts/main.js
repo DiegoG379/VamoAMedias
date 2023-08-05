9af5c83f8f8daaa295da9b4a2080444e2c79ea26
Diego Garcia <diegofing379@gmail.com>
2023-08-04 23:14:56 -0300

    V3.0.0 Se realiza la logica para dividir los gastos entre dos y tres personas, se comienza a implementar guardado de datos con JSON.

--- scripts/main.js
+++ scripts/main.js
@@ -21,6 +21,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // Ingreso para tres personas - Final
 
     // Ingreso y gestión de personas y nombres - Inicio
+    let nombres = [];
 
     function guardarNombres() {
         const cantidadPersonas = document.getElementById("cantidadPersonas");
@@ -28,7 +29,7 @@ document.addEventListener("DOMContentLoaded", function () {
         const nombreUno = document.getElementById("nombreUno").value;
         const nombreDos = document.getElementById("nombreDos").value;
 
-        let nombres = [];
+        nombres = [];
         let nombreSeleccionado = "";
 
         nombres.push(nombreUno);
@@ -106,7 +107,7 @@ document.addEventListener("DOMContentLoaded", function () {
         gastos.forEach((gasto, indice) => {
             const elementoLista = document.createElement("li");
             const textoElemento = document.createTextNode(
-                `${gasto.nombre} ${gasto.item} a $${gasto.precio}`
+                `${gasto.nombre} compró ${gasto.item} a $${gasto.precio}`
             );
             const botonEliminarGasto = document.createElement("button");
             botonEliminarGasto.textContent = "Eliminar";
@@ -120,8 +121,183 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
     // Ingreso y gestión de articulos, precios y personas que hicieron la compra - Final
+    
+    // Se calculan gastos y muestra resultados - Inicio
+    let listaDeGastos = {};
+
+    function calcularGasto(){
+        listaDeGastos = {};
+
+        for (const gasto of gastos) {
+            const nombre = gasto.nombre;
+            const precio = parseInt(gasto.precio);
+            if (nombre in listaDeGastos){
+                listaDeGastos[nombre] += precio;
+            } else{
+                listaDeGastos[nombre] = precio;
+            }
+        }
+
+        let nombreUno = nombres[0]; 
+        let gastoUno = 0;
+        let nombreDos = nombres[1];
+        let gastoDos = 0;
+        let nombreTres = nombres[2];
+        let gastoTres = 0;
+
+        let verificador = 0;
+
+        for (const nombre in listaDeGastos){
+            const gastoPorPersona = listaDeGastos[nombre];
+            if (verificador === 0){
+                gastoUno = gastoPorPersona;
+            } else if (verificador === 1) {
+                gastoDos = gastoPorPersona;
+            } else if (verificador === 2) {
+                gastoTres = gastoPorPersona;
+            }
+            verificador++;
+        }
+
+        if (verificarTres === false){
+            let mensaje = "";
+            let gastoTotal = gastoUno + gastoDos;
+
+            let gastoMedioUno = gastoUno / 2; 
+            let gastoMedioDos = gastoDos / 2;
+            
+            let totalUno = gastoMedioUno - gastoMedioDos;
+            let totalDos = gastoMedioDos - gastoMedioUno;
+
+            if (totalUno < 0) {
+                mensaje = `${nombreUno} le debe a ${nombreDos}, $${totalDos}`;
+            } else if (totalDos < 0 ) {
+                mensaje = `${nombreDos} le debe a ${nombreUno}, $${totalUno}`;
+            } else if (totalUno === totalDos) {
+                mensaje = `${nombreUno} y ${nombreDos}, están a mano`;
+            }
+
+            const resultados = document.getElementById("resultados");
+            resultados.innerHTML = "";
+
+            const parrafo = document.createElement("p");
+            const textroUno = document.createTextNode(`El total gastado es de $${gastoTotal}.`);
+            const lineaBr = document.createElement("br");
+            const lineaBrDos = document.createElement("br");
+            const lineaBrTres = document.createElement("br");
+            const textroDos = document.createTextNode(`${nombreUno} aportó $${gastoUno} y ${nombreDos} aportó $${gastoDos}.`);
+            const textroTres = document.createTextNode(mensaje);
+
+            parrafo.appendChild(textroUno);
+            parrafo.appendChild(lineaBr);
+            parrafo.appendChild(textroDos);
+            parrafo.appendChild(lineaBrDos);
+            parrafo.appendChild(lineaBrTres);
+            parrafo.appendChild(textroTres);
+
+            resultados.appendChild(parrafo);
+        }
+        
+        if (verificarTres === true) {
+            let mensaje = "";
+            let mensajeDos = "";
+            let gastoTotal = gastoUno + gastoDos + gastoTres;
+
+            let totalUno = (gastoTotal/3 - gastoUno); 
+            let totalDos = (gastoTotal/3 - gastoDos);
+            let totalTres = (gastoTotal/3 - gastoTres);
+
+            if (gastoUno === gastoDos && gastoUno === gastoTres) {
+                mensaje = `${nombreUno}, ${nombreDos} y ${nombreTres} están a mano`;
+            } else if (totalUno >= 0 && totalDos >= 0) {
+                mensaje = `${nombreUno} le debe a ${nombreTres}, $${totalUno}`;
+                mensajeDos = `${nombreDos} le debe a ${nombreTres}, $${totalDos}`;
+            } else if (totalDos >= 0 && totalTres >= 0) {
+                mensaje = `${nombreDos} le debe a ${nombreUno}, $${totalDos}`;
+                mensajeDos = `${nombreTres} le debe a ${nombreUno}, $${totalTres}`;
+            } else if (totalUno >= 0 && totalTres >= 0) {
+                mensaje = `${nombreUno} le debe a ${nombreDos}, $${totalUno}`;
+                mensajeDos = `${nombreTres} le debe a ${nombreDos}, $${totalTres}`;
+            } else if (totalUno <= 0 && totalDos <= 0) {
+                mensaje = `${nombreTres} le debe a ${nombreUno}, $${-totalUno}`;
+                mensajeDos = `${nombreTres} le debe a ${nombreDos}, $${-totalDos}`;
+            } else if (totalDos <= 0 && totalTres <= 0) {
+                mensaje = `${nombreUno} le debe a ${nombreDos}, $${-totalDos}`;
+                mensajeDos = `${nombreUno} le debe a ${nombreTres}, $${-totalTres}`;
+            } else if (totalUno <= 0 && totalTres <= 0) {
+                mensaje = `${nombreDos} le debe a ${nombreUno}, $${-totalUno}`;
+                mensajeDos = `${nombreDos} le debe a ${nombreTres}, $${-totalTres}`;
+            }
+
+            const resultados = document.getElementById("resultados");
+            resultados.innerHTML = "";
+
+            const parrafo = document.createElement("p");
+            const textroUno = document.createTextNode(`El total gastado es de $${gastoTotal}.`);
+            const lineaBr = document.createElement("br");
+            const lineaBrDos = document.createElement("br");
+            const lineaBrTres = document.createElement("br");
+            const lineaBrCuatro = document.createElement("br");
+            const textroDos = document.createTextNode(`${nombreUno} aportó $${gastoUno}, ${nombreDos} aportó $${gastoDos} y ${nombreTres} aportó $${gastoTres}.`);
+            const textroTres = document.createTextNode(mensaje);
+            const textroCuatro = document.createTextNode(mensajeDos);
+
+            parrafo.appendChild(textroUno);
+            parrafo.appendChild(lineaBr);
+            parrafo.appendChild(textroDos);
+            parrafo.appendChild(lineaBrDos);
+            parrafo.appendChild(lineaBrTres);
+            parrafo.appendChild(textroTres);
+            parrafo.appendChild(lineaBrCuatro);
+            parrafo.appendChild(textroCuatro);
+
+            resultados.appendChild(parrafo);
+        }
+    }
+    // Se calculan gastos y muestra resultados - Final
 
     document.getElementById("cantidadPersonas").addEventListener("change", agregarTercerPersona);
     document.getElementById("ingresar").addEventListener("click", guardarNombres);
     document.getElementById("agregar").addEventListener("click", agregarGasto);
+    document.getElementById("calcular").addEventListener("click", calcularGasto);
 });
+
+// Se guardan y recuperan los datos en los campos ingresados - Inicio
+const formulario = document.querySelector('#formulario');
+const botonGuardar = document.querySelector('#guardar');
+
+botonGuardar.addEventListener('click', function(){
+    const guardarCampos = formulario.querySelectorAll('input, select');
+    const guardarValores = {};
+
+    guardarCampos.forEach(function (campo) {
+        guardarValores[campo.name] = campo.value;
+    });
+
+    const cantidadPersonasSelect = document.querySelector('#cantidadPersonas');
+    guardarValores[cantidadPersonasSelect.name] = cantidadPersonasSelect.options[cantidadPersonasSelect.selectedIndex].value;
+
+    const PersonasSelect = document.querySelector('#Personas');
+    guardarValores[PersonasSelect.name] = PersonasSelect.options[PersonasSelect.selectedIndex].value;
+
+    localStorage.setItem('valoresGuardados', JSON.stringify(guardarValores));
+
+    mostrarvaloresGuardados();
+})
+
+function mostrarvaloresGuardados(){
+    const valoresGuardados = localStorage.getItem('valoresGuardados');
+
+    if (valoresGuardados) {
+        const RecuperarvaloresGuardados = JSON.parse(valoresGuardados);
+
+        Object.keys(RecuperarvaloresGuardados).forEach(function(nombreCampo) {
+            const valor = formulario.querySelector(`[name="${nombreCampo}"]`);
+            if (valor) {
+                valor.value = RecuperarvaloresGuardados[nombreCampo];
+            }
+        })
+    }
+}
+mostrarvaloresGuardados();
+// Se guardan y recuperan los datos en los campos ingresados - Final
